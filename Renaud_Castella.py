3116bd37bda0bd9c4a633dbea86e8650b68d6e18
sylvain.renaud <sylvain.renaud@he-arc.ch>
2017-11-08 15:48:10 +0100

    Squellette du code + update gitignore pour pycharm.

---
+++ Renaud_Castella.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+
+def ga_solve(file=None, gui=True, maxtime=0):
+    if file == None:
+        # ouvrir la fenetre pour entrer les points manuellement
+        pass
+    else:
+        #lire le fichier
+        pass
