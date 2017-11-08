9620b1a7cc0bfac05df681586ba0b080235d7a5e
sylvain.renaud <sylvain.renaud@he-arc.ch>
2017-11-08 17:44:30 +0100

    Extract datas from files. data files and stuff

--- Renaud_Castella.py
+++ Renaud_Castella.py
@@ -2,10 +2,31 @@ import pygame
 import sys
 
 
-def ga_solve(file=None, gui=True, maxtime=0):
-    if file == None:
+class Ville():
+    def __init__(self, x, y, name=""):
+        self.x = x
+        self.y = y
+        self.name = name
+
+    def __str__(self):
+        return f"[{self.x},{self.y},{self.name}]"
+
+    def __repr__(self):
+        return f"[{self.x},{self.y},{self.name}]"
+
+
+def ga_solve(filename=None, gui=True, maxtime=0):
+    villes = []
+    if filename == None:
         # ouvrir la fenetre pour entrer les points manuellement
         pass
     else:
-        #lire le fichier
-        pass
+        # lire le fichier
+        with open(filename) as file:
+            for line in file:
+                name, x, y = line.split()
+                villes.append(Ville(int(x), int(y), name))
+            print(len(villes), villes)
+
+
+ga_solve("data/pb010.txt", False)
